75c2160b3f71be580da47f40b446bc596f7f4116
Joki02 <bjalao@utpl.edu.ec>
2023-10-26 17:42:56 -0500

    4TO EJERCICIO RESUELTO

---
+++ Ejercicio 04/Ejercicio 04/src/ejercicio/pkg04/Ejercicio04.java
@@ -0,0 +1,17 @@
+
+package ejercicio.pkg04;
+public class Ejercicio04 {
+
+    public static void main(String[] args) {
+    int var1 = 20;
+    int var2 = 1;
+    int var3 = 2;
+    int var4 = 3;  
+    boolean respuesta;
+    
+    respuesta = (var1/(var1 + var4)+ var3) >= 3;
+    System.out.println("La respuesta es: " + respuesta);
+    //Autor BJALAO
+    }
+    
+}
